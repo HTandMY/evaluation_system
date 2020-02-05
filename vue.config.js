f754e00c77296432f794269a720193ba4fb339fe
htandmy <htandmy@qq.com>
2020-02-06 00:42:56 +0900

    ver 0.10.0

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/evaluation_system/'
-      : '/'
+      : '/',
+    pwa: {
+        iconPaths: {
+        favicon32: 'favicon.png',
+        favicon16: 'favicon.png',
+        appleTouchIcon: 'favicon.png',
+        maskIcon: 'favicon.png',
+        msTileImage: 'favicon.png'
+        }
+    }
 }
